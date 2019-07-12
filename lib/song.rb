cd71cdf7ba236c1eff2d5505154ddb673475ad68
Jack Atherton <jackrrye@gmail.com>
2019-07-12 22:29:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,21 @@
 require "pry"
 
 class Song
-
+  
   attr_accessor :name, :artist
-
+  
   def initialize(name)
     @name = name
-  end
-
+  end 
+  
   def self.new_by_filename(filename)
     details = filename.split(" - ")
     song = self.new(details[1])
-    song.artist = Artist.find_or_create_by_name(details[0])
+    artist = Artist.find_or_create_by_name(details[0])
+    song.artist = artist
     artist.add_song(song)
     song
-  end
-
-
+  end 
+    
+ 
 end
